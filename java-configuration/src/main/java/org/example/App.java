bf2ea7dfb0990c9b8815da1b1d938fb0c34586e4
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-16 14:01:47 +0530

    Add files via upload

---
+++ java-configuration/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
+        Employee employee = context.getBean("employee",Employee.class);
+        employee.print();
+        System.out.println(employee);
+
+    }
+}
