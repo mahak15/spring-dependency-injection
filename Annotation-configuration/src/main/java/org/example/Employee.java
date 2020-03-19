701c346f6041ba5c38fdb831b7ae32cc66655e9c
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-19 16:45:58 +0530

    Update Employee.java

--- Annotation-configuration/src/main/java/org/example/Employee.java
+++ Annotation-configuration/src/main/java/org/example/Employee.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:src/main/java/org/example/Employee.java
+
 package org.example;
 
 
@@ -57,62 +57,3 @@ public class Employee {
                 '}';
     }
 }
-=======
-package org.example;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
-import java.util.LinkedList;
-import java.util.List;
-
-@Component
-@PropertySource("Employee.properties")
-public class Employee {
-    @Value("${name}")
-    private String name;
-    @Value("${age}")
-    private int age;
-    @Value("${salary}")
-    private Double salary;
-    @Value("${list1}")
-    private String list1;
-    @Value("${list2}")
-    private String list2;
-    @Value("${list3}")
-    private  String list3;
-
-    private List<String> taskList;
-    public void setTaskList() {
-        List<String> list = new LinkedList<String>();
-        list.add(list1);
-        list.add(list2);
-        list.add(list3);
-        this.taskList = list;
-    }
-
-    @Autowired
-    private Department department;
-
-    public Employee() {
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", salary=" + salary +
-                ", department=" + department +
-                " : TaskList is as follows : " + taskList +
-                '}';
-    }
-}
->>>>>>> e00b2eec65a07d397db4071a466c752d53f763cb:Annotation-configuration/src/main/java/org/example/Employee.java
