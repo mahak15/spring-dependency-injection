090a71f62c9d83a9212fd07dc4f97a115c4017d7
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-19 16:44:50 +0530

    Update DateFormat.java

--- Annotation-configuration/src/main/java/org/example/DateFormat.java
+++ Annotation-configuration/src/main/java/org/example/DateFormat.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:Annotation-configuration/src/main/java/org/example/DateFormat.java
+
 package org.example;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -23,23 +23,3 @@ public class DateFormat {
         }
         return parseDate;
     }
-=======
-package org.example;
-import org.springframework.stereotype.Component;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-@Component(value = "stringToDate")
-public class StringToDate {
-    public  static Date parse(String pattern, String currentDate) {
-        Date parseDate= null;
-        try {
-            parseDate = new SimpleDateFormat(pattern).parse(currentDate);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return parseDate;
-    }
->>>>>>> e00b2eec65a07d397db4071a466c752d53f763cb:Annotation-configuration/src/main/java/org/example/StringToDate.java
-}
