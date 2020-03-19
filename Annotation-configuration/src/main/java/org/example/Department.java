e3a7bf93789210afb3f766fdbdced5b79bab9605
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-19 16:45:28 +0530

    Update Department.java

--- Annotation-configuration/src/main/java/org/example/Department.java
+++ Annotation-configuration/src/main/java/org/example/Department.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:src/main/java/org/example/Department.java
+
 package org.example;
 
 
@@ -37,37 +37,3 @@ public class Department {
         return  departmentName+ " : created on : "+ createdOn;
     }
 }
-=======
-package org.example;
-
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
-@Component
-@PropertySource("Employee.properties")
-public class Department {
-
-    @Value("${departmentName}")
-    private String departmentName;
-
-    @Value("${dateFormat}")
-    private String pattern;
-
-    @Value("${date}")
-    private String currentDate;
-
-    private Date createdOn;
-
-    public void setCreatedOn() {
-        createdOn = StringToDate.parse(pattern, currentDate);
-    }
-
-    @Override
-    public String toString() {
-        return  departmentName+ " : created on : "+ createdOn;
-    }
-}
->>>>>>> e00b2eec65a07d397db4071a466c752d53f763cb:Annotation-configuration/src/main/java/org/example/Department.java
