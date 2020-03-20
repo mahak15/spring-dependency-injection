9e9931074435abfb9973b491a53138823b8c43d3
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-20 15:59:16 +0530

    Update App.java

--- Annotation-configuration/src/main/java/org/example/App.java
+++ Annotation-configuration/src/main/java/org/example/App.java
@@ -1,15 +1,17 @@
-
 package org.example;
+
+
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App {
-    public static void main(String[] args) {
-       ApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
-       Employee emp= (Employee) context.getBean("employee");
-       emp.setTaskList();
-     System.out.println(emp);
+        public class App {
 
+            public static void main(String[] args) {
+                ApplicationContext context = new ClassPathXmlApplicationContext("applicationContent.xml");
+                Employee employee = context.getBean(Employee.class);
+                employee.displayInfo();
 
-    }
-}
+            }
+
+        }
