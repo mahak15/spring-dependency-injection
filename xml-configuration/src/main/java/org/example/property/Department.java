19715e6aabc96c8f9ea7d9bbf4a28369e09a2e2a
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-20 17:03:43 +0530

    Delete Department.java

--- xml-configuration/src/main/java/org/example/property/Department.java
+++
@@ -1,8 +0,0 @@
-package org.example.property;
-
-public class Department implements provideService{
-    @Override
-    public void service() {
-System.out.println("this department provide Software tools");
-    }
-}
