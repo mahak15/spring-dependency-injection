55c729db3fc9d23f4a11aec2fc31edddbdacf7c4
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-20 17:03:59 +0530

    Delete Employee.java

--- xml-configuration/src/main/java/org/example/property/Employee.java
+++
@@ -1,9 +0,0 @@
-package org.example.property;
-
-public class Employee implements provideService {
-
-    @Override
-    public void service() {
-System.out.println("this employee provide Software tools as well as Product support ");
-    }
-}
